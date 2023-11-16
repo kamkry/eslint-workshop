246fef58fb616f492dbbdc35d953bb13923aa84a
Kamil Krysiak <kamkry99@gmail.com>
2023-11-16 09:56:00 +0100

    Fixes setup (#1)

--- eslint-plugin-workshop/jest.config.js
+++ eslint-plugin-workshop/jest.config.js
@@ -1,5 +1,5 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  reporters: ['default', 'jest-junit'],
+  modulePathIgnorePatterns: ['dist']
 };
